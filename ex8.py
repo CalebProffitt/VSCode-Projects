4e35571c5596f716bbd472c0525079e8440d53c5
CalebProffitt <calebproffitt97@gmail.com>
2020-07-14 22:28:51 -0400

    Ex 6-8

---
+++ ex8.py
@@ -0,0 +1,12 @@
+f = "{} {} {} {}"  # formatter
+
+print(f.format(1, 2, 3, 4))
+print(f.format("one", "two", "three", "four"))
+print(f.format(True, False, False, True))
+print(f.format(f, f, f, f))
+print(f.format(
+    "Try your",
+    "Own text",
+    "Maybe a poem",
+    "Or a song about fear"
+))
